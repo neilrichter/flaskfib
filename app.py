5ca06fea857e086a0b03d7be93ed37c05a11378d
neilrichter <me@neilrichter.com>
2018-10-27 18:33:35 +0200

    Edited deployment configuration file

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/fib/<int:x>')
+def fib(x):
+	a, b = 0, 1
+	for _ in range(x):
+		a, b = b, a+b
+	return str(b)
+
+if __name__ == '__main__':
+	app.run()
