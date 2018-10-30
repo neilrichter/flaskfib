cc60bb89ba01f7d8431ab77c64b5c74bb924d0c4
neilrichter <me@neilrichter.com>
2018-10-30 14:55:24 +0100

    Linted with black

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/fib/<int:x>')
-def fib(x):
-	a, b = 0, 1
-	for _ in range(x):
-		a, b = b, a+b
-	return str(b)
 
-if __name__ == '__main__':
-	app.run(port=8000)
+@app.route("/fib/<int:x>")
+def fib(x):
+    a, b = 0, 1
+    for _ in range(x):
+        a, b = b, a + b
+    return str(b)
+
+
+if __name__ == "__main__":
+    app.run(port=8000)
+
